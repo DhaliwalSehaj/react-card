90aaebdeeffd7b018351477ee6def3d39bc3c83a
DhaliwalSehaj <120234457+DhaliwalSehaj@users.noreply.github.com>
2024-02-27 05:02:22 -0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,25 @@
+const productData = [
+    {
+      id: 1,
+      name: "Basement for Rent",
+      image:"https://firebasestorage.googleapis.com/v0/b/calgary-help.appspot.com/o/files%2F2b246114-25bc-4e22-806e-12d18abcf898?alt=media&token=3a4534ab-84cf-49e4-916d-34f21ddf935c",
+      productType: "openBackHeadphones",
+      price: 220,
+    },
+    {
+      id: 2,
+      name: "House for Sale",
+      image:"https://media.istockphoto.com/id/1323076054/photo/luxury-home-calgary.jpg?s=612x612&w=0&k=20&c=aYke93jyJLJeXbZ21a_R7WDabS965K_8z-s0xaoOd58=",
+      productType: "openBackHeadphones",
+      price: 1069000,
+      
+    },
+    {
+      id: 3,
+      name: "Bedroom available for rent",
+      image:"https://media.gettyimages.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=gi&k=20&c=ANSbtbaJgd_AQYLH0f7gp2eLgzXdal6UZrw3Lxqgeok=",
+      productType: "l",
+      price: 170
+    } 
+  ];
+  export default productData;
